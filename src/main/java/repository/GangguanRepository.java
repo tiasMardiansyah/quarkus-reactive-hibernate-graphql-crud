aec2e04eaf7d91c5bd1fe1d38bdb260365db646c
tiasMardiansyah <tiasmardiansyah5@gmail.com>
2024-01-05 20:05:18 +0700

    perbaikan struktur kode

--- src/main/java/repository/GangguanRepository.java
+++ src/main/java/repository/GangguanRepository.java
@@ -1,11 +1,7 @@
 package repository;
 
-
-import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -16,38 +12,8 @@ import io.smallrye.mutiny.Uni;
 @ApplicationScoped
 public class GangguanRepository implements PanacheRepositoryBase<Gangguan, Long> {
 
-    public Uni<List<Gangguan>> customSearch(
-        Optional<Integer> status, 
-        Optional<LocalDate> tglAwal, 
-        Optional<LocalDate> tglAkhir
-    ) {
-        StringBuilder query = new StringBuilder("1=1 ");
-        Map<String,Object> parameter = new HashMap<>();
-
-        if (!status.isEmpty()) {
-            query.append("AND status = :status ");
-            parameter.put("status", status.get());
-        }
-
-        if (!tglAwal.isEmpty() && !tglAkhir.isEmpty()) {
-            query.append("AND date(tgl) between :tgl_awal and :tgl_akhir ");
-            parameter.put("tgl_awal", tglAwal.get());
-            parameter.put("tgl_akhir", tglAkhir.get());
-
-        } else if (!tglAwal.isEmpty()) {
-            query.append("AND date(tgl) >= :tgl_awal ");
-            parameter.put("tgl_awal", tglAwal.get());
-
-        } else if (!tglAkhir.isEmpty()) {
-
-            query.append("AND date(tgl) <= :tgl_akhir ");
-            parameter.put("tgl_akhir", tglAkhir.get());
-
-        }
-
+    public Uni<List<Gangguan>> customSearch(String query, Map<String,Object> parameter) {
         return find(query.toString(), parameter).list();
-
     }
 
-    // public Uni<List<Gangguan>
 }
--- src/main/java/service/GangguanService.java
+++ src/main/java/service/GangguanService.java
@@ -4,8 +4,9 @@ package service;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Collections;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -37,6 +38,28 @@ public class GangguanService {
         Optional<LocalDate> tglAkhir
     ) {
 
-        return gangguanRepository.customSearch(status,tglAwal,tglAkhir);
+        StringBuilder query = new StringBuilder("1=1 ");
+        Map<String,Object> parameter = new HashMap<>();
+
+        if (!status.isEmpty()) {
+            query.append("AND status = :status ");
+            parameter.put("status", status.get());
+        }
+
+        if (!tglAwal.isEmpty() && !tglAkhir.isEmpty()) {
+            query.append("AND date(tgl) between :tgl_awal and :tgl_akhir ");
+            parameter.put("tgl_awal", tglAwal.get());
+            parameter.put("tgl_akhir", tglAkhir.get());
+
+        } else if (!tglAwal.isEmpty()) {
+            query.append("AND date(tgl) >= :tgl_awal ");
+            parameter.put("tgl_awal", tglAwal.get());
+
+        } else if (!tglAkhir.isEmpty()) {
+            query.append("AND date(tgl) <= :tgl_akhir ");
+            parameter.put("tgl_akhir", tglAkhir.get());
+        }
+
+        return gangguanRepository.customSearch(query.toString(),parameter);
     }
 }
